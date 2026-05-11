d9ce30f1f53b3c3562cd77bd30797cca33a0b521
pentacore-demo <pentacore.support@gmail.com>
2026-05-11 16:15:24 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
+<<<<<<< HEAD
 const admin = require('firebase-admin');
 
 // Prevent multiple initialization
 if (!admin.apps.length) {
-    const serviceAccount = require("./fir-c1b0e-firebase-adminsdk-fbsvc-ba4d8926e8.json");
+    const serviceAccount = require("./fir-c1b0e-firebase-adminsdk-fbsvc-052b9da7d2.json");
 
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
@@ -15,4 +16,23 @@ if (!admin.apps.length) {
 
 const db = admin.database();
 
+=======
+const admin = require('firebase-admin');
+
+// Prevent multiple initialization
+if (!admin.apps.length) {
+    const serviceAccount = require("./fir-c1b0e-firebase-adminsdk-fbsvc-052b9da7d2.json");
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+
+        // ⚠️ MUST MATCH serviceAccount.project_id
+        databaseURL: "https://fir-c1b0e-default-rtdb.asia-southeast1.firebasedatabase.app"
+    });
+}
+
+
+const db = admin.database();
+
+>>>>>>> fc30a8b4245210df0553ef4b1594fd34e3ed29ae
 module.exports = db;
